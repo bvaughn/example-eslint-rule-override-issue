b62b2f0c02f219b445084ce4ed3ab025ceac458f
Brian Vaughn <brian.david.vaughn@gmail.com>
2018-01-09 10:37:09 -0800

    Fixed conditional rule to work

--- .eslintrc.js
+++ .eslintrc.js
@@ -11,10 +11,6 @@ module.exports = {
   ],
 
   rules: {
-    // Turning this rule on here runs it for all tests.
-    // Turning it OFF (or omitting it) suppresses it for all tests,
-    // Despite the below override.
-    'example-internal/no-focused-tests': OFF,
   },
 
   overrides: [
